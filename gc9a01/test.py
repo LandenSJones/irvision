94fa0e30450314b778b3cf2fba5ff93e39dd0c74
Landen <landensjones@gmail.com>
2025-01-29 20:40:25 -0600

    test update

--- gc9a01/test.py
+++ gc9a01/test.py
@@ -1,24 +1,15 @@
 import ST7789 as ST7789
-import RPi.GPIO as GPIO
 from PIL import Image
 
-RST_PIN = 25  # GPIO pin for Reset (Pin 22)
-
-# Initialize Reset Pin
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RST_PIN, GPIO.OUT)
-GPIO.output(RST_PIN, GPIO.HIGH)
-
-# Initialize SPI Display
+# Initialize display
 disp = ST7789.ST7789(
     port=0,
-    cs=0,  # Chip Select (Pin 24, GPIO8)
-    dc=24, # Data/Command (Pin 18, GPIO24)
-    rst=25, # Reset (Pin 22, GPIO25)
-    backlight=None,
+    cs=0,  # Chip Select (CS0, GPIO8)
+    dc=24, # Data/Command (GPIO24)
+    rst=25, # Reset (GPIO25)
     width=240,
     height=240,
-    rotation=180,
+    rotation=180,  # Adjust if needed
     spi_speed_hz=40000000
 )
 
