661a92c9b430c320e0b88eef117d25c849f43d77
Landen <landensjones@gmail.com>
2025-02-04 10:58:04 -0600

    l'

--- video/video.py
+++ video/video.py
@@ -14,14 +14,14 @@ display = ST7789(
 )
 
 
-buffer = Image.new("RGB", (320, 240))
-draw = ImageDraw.Draw(buffer)
+#buffer = Image.new("RGB", (320, 240))
+#draw = ImageDraw.Draw(buffer)
 
-draw.rectangle((0, 0, 50, 50), (255, 0, 0))
-draw.rectangle((320 - 50, 0, 320, 50), (0, 255, 0))
-draw.rectangle((0, 240 - 50, 50, 240), (0, 0, 255))
-draw.rectangle((320 - 50, 240 - 50, 320, 240), (255, 255, 0))
+#draw.rectangle((0, 0, 50, 50), (255, 0, 0))
+#draw.rectangle((320 - 50, 0, 320, 50), (0, 255, 0))
+#draw.rectangle((0, 240 - 50, 50, 240), (0, 0, 255))
+#draw.rectangle((320 - 50, 240 - 50, 320, 240), (255, 255, 0))
 
-sleep(0.1)
+#sleep(0.1)
 
-display.display(buffer)
+#display.display(buffer)
