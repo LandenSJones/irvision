63f6664adaa0977e4a03199b7f22ab2be3342165
Landen <landensjones@gmail.com>
2025-02-04 10:03:32 -0600

    try display image

--- video/video.py
+++ video/video.py
@@ -1,4 +1,5 @@
 from st7789 import ST7789
+from PIL import Image, ImageDraw
 
 display = ST7789(
     port=0,
@@ -10,3 +11,14 @@ display = ST7789(
     rotation=180,
     spi_speed_hz=60 * 1000 * 1000,
 )
+
+
+buffer = Image.new("RGB", (320, 240))
+draw = ImageDraw.Draw(buffer)
+
+draw.rectangle((0, 0, 50, 50), (255, 0, 0))
+draw.rectangle((320 - 50, 0, 320, 50), (0, 255, 0))
+draw.rectangle((0, 240 - 50, 50, 240), (0, 0, 255))
+draw.rectangle((320 - 50, 240 - 50, 320, 240), (255, 255, 0))
+
+display.display(buffer)
